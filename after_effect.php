1d72b97cd7cca55a38303ece8caa2a65550b7e36
hantaehee <genius_han@nate.com>
2021-09-09 18:28:44 +0900

    up

---
+++ after_effect.php
@@ -0,0 +1,16 @@
+<?
+    // 색깔 animate
+    Effect => Color Correction => Hue/Saturation
+
+    // 팡! animate
+    Use Shape Layer; => Trim Paths start, and => Add => Repeater => Rotation 360/Copies
+
+    // Text animate (Up)
+    TEXT => Add => Position => Range Selector => Offset 
+    option : Advenced => Shape => Square:기본값 하나하나, Ramp Up:겹쳐서
+                        Ease Low : 부드럽게
+
+    // line effect
+    Trim Path Animate or Path key frame
+
+?>
