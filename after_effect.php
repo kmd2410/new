67963be0b9d8716dc02ad6f4bfc12126e15c9c4c
hantaehee <genius_han@nate.com>
2021-09-14 18:53:35 +0900

    up

--- after_effect.php
+++ after_effect.php
@@ -13,4 +13,45 @@
     // line effect
     Trim Path Animate or Path key frame
 
+    // Particle 화염효과?
+    Effects&Presets => CC Particle World
+    
+
+    ct+at+Y : 조정레이어
+    
+    // Glow 광선
+
+    //익스프레션
+    //1) 크기
+    A=thisComp.layer("Text Layer")
+    B=A.sourceRectAtTime().width;
+
+    [B+100,60]
+
+    //2) 따라가기 ?
+    C=thisComp.layer("Shape Layer")
+    D=C.sourceRectAtTime();
+
+    C.toComp([D.left,D.top-30])
+
+    // 1개의 텍스트 여러개로 보이게
+    Effects&Presets => Set Channels;
+    
+    // Text 윤곽선만 남게하기
+    Effects&Presets => Find Edges;
+
+    // Text 자간 애니메이션
+    Anmation => Tracking => Tracking Amount 
+
+    // Text 뽈록
+    Effects&Presets => Emboss;
+
+    // Text 배경과 글자색
+    Tint;
+
+    // 블러.?
+    CC Radial Blur; Fast Bot Blur;
+
+    // Text 글자 쪼개기
+    Transform, Compositing Options.......
 ?>
