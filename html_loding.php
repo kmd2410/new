241d9ea21ab74c8eee46b209ee638b76d4940541
hantaehee <75105354+kmd2410@users.noreply.github.com>
2021-10-06 23:54:35 +0900

    up

---
+++ html_loding.php
@@ -0,0 +1,84 @@
+<script>
+    // 함수 실행시 파일을 로딩하여 보여줌
+
+    function LoadingWithMask(){
+        // 화면의 높이와 너비
+        let maskHeight = $(document).height();
+        let maskWidth = window.document.body.clientWidth;
+
+        // 화면 출력 마스크 설정
+        let mask = "<div id='mask' style='position:absolute; z-index:9999; background-color:#000000; display:none; left:0; top:0;'></div>";
+        let loadingImg = "";
+
+        loadingImg += "<div id='loadingImg'>";
+        loadingImg += "<img src='LoadingImg.gif' style='position: relative; display:block; margin:0px auto;'/>";
+        loadingImg += "</div>";
+
+        // 화면 레이어 추가
+        $("body").append(mask).append(loadingImg);
+
+        // 마스크의 높이와 너비를 전체화면으로 설정
+        $("#mask").css({
+            "width" : maskWidth,
+            "height" : maskHeight,
+            "opacity" : "0.3"
+        });
+
+        // 마스크 표시
+        $("#mask").shwo();
+
+        // 로딩중 이미지 표시
+        $("#loadingImg").show();
+    }
+
+
+    // 로딩중 표시 제거 함수
+    function closeLoadingWithMask(){
+        $("#mask, #loadingImg").hide();
+        $("#mask, #loadingImg").remove();
+    }
+
+
+
+    ///////////////////////////////////
+    function test(imageName) {
+    LoadingWithMask('your site\'s image path');
+    setTimeout("closeLoadingWithMask()", 3000);
+    }
+
+    function LoadingWithMask(gif) {
+        //화면의 높이와 너비를 구합니다.
+        var maskHeight = $(document).height();
+        var maskWidth  = window.document.body.clientWidth;
+        
+        //화면에 출력할 마스크를 설정해줍니다.
+        var mask       ="<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+        var loadingImg ='';
+
+        loadingImg +=" <img src='"+ gif +"' style='position: absolute; display: block; margin: 0px auto;'/>";
+
+        //화면에 레이어 추가
+        $('body')
+            .append(mask)
+
+        //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+        $('#mask').css({
+                'width' : maskWidth,
+                'height': maskHeight,
+                'opacity' :'0.3'
+        });
+
+        //마스크 표시
+        $('#mask').show();
+
+        //로딩중 이미지 표시
+        $('#loadingImg').append(loadingImg);
+        $('#loadingImg').show();
+    }
+
+    function closeLoadingWithMask() {
+        $('#mask, #loadingImg').hide();
+        $('#mask, #loadingImg').empty(); 
+    }
+
+</script>
