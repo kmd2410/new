298200ba5adae8bb01a82c73784f50b126e9420b
hantaehee <genius_han@nate.com>
2021-10-08 18:59:54 +0900

    up

---
+++ family_site.php
@@ -0,0 +1,4 @@
+<?
+    "https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=7222hello&logNo=220738749707";
+    
+?>
---
+++ php/경로.php
@@ -0,0 +1,5 @@
+<?
+    $_SERVER["DOCUMENT_ROOT"] // 현재 프로젝트의 루트 경로
+
+    echo getcwd();
+?>
